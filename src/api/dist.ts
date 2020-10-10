c6f8029b6c30b9fad62e196ac99f0407af56891a
Theo Sun <theo.sun@outlook.com>
2020-10-10 14:58:54 +0800

    chore: with concurrency limit

--- src/api/dist.ts
+++ src/api/dist.ts
@@ -1,3 +1,4 @@
+import { concurrency } from "@newdash/newdash/concurrency";
 import fetch from "node-fetch";
 
 export interface PackageDistQueryResult {
@@ -5,15 +6,14 @@ export interface PackageDistQueryResult {
   [key: string]: string;
 }
 
-
-export async function queryPackageDistTag(
+export const queryPackageDistTag = concurrency.limit(async (
   packageName: string,
   registry = 'https://registry.npmjs.org/'
-): Promise<PackageDistQueryResult> {
+): Promise<PackageDistQueryResult> => {
   const res = await fetch(`${registry}-/package/${packageName}/dist-tags`);
   if (res.status != 200) {
     throw new Error(await res.text());
   } else {
     return await res.json();
   }
-}
+}, 5);
