62df590475fff0f415b0fb29fbed176709d41c9b
Theo Sun <theo.sun@outlook.com>
2020-10-10 15:01:59 +0800

    chore: update source format

--- src/api/dist.ts
+++ src/api/dist.ts
@@ -1,19 +1,22 @@
 import { concurrency } from "@newdash/newdash/concurrency";
 import fetch from "node-fetch";
 
+const DEFAULT_REGISTRY = 'https://registry.npmjs.org/';
+
 export interface PackageDistQueryResult {
   latest: string;
   [key: string]: string;
 }
 
-export const queryPackageDistTag = concurrency.limit(async (
-  packageName: string,
-  registry = 'https://registry.npmjs.org/'
-): Promise<PackageDistQueryResult> => {
-  const res = await fetch(`${registry}-/package/${packageName}/dist-tags`);
-  if (res.status != 200) {
-    throw new Error(await res.text());
-  } else {
-    return await res.json();
-  }
-}, 5);
+
+export const queryPackageDistTag = concurrency.limit(
+  async (packageName: string, registry = DEFAULT_REGISTRY): Promise<PackageDistQueryResult> => {
+    const res = await fetch(`${registry}-/package/${packageName}/dist-tags`);
+    if (res.status != 200) {
+      throw new Error(await res.text());
+    } else {
+      return await res.json();
+    }
+  },
+  5
+);
